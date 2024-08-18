9d1e4260a73e12743e798a5d8c4114a411ad24c7
FakuPara <wilkeoncken@protonmail.com>
2024-08-18 23:32:41 +0200

    works

---
+++ AnimatedText.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+interface IProps {
+  text:string,
+  fontSize?:number
+}
+const props = withDefaults(defineProps<IProps>(),{
+  text:"",
+  fontSize:6,
+  speed: 0.01
+})
+
+const dLetter = 0.02;
+const lines:string[] = splitTextIntoLines(props.text)
+const dLines = [0, ...lines.map((line)=> line.length * dLetter)]
+const linesRef:Ref<string[]> = ref([lines]);
+
+console.log("TEXT",props.text)
+function splitTextIntoLines(text:string) {
+  return text.split("\n");
+}
+
+function getAnimationDelay(letterIndex:number, lineIndex:number) {
+  const dLetter = props.speed;
+
+  const delay = letterIndex * dLetter + "s";
+  console.log(letterIndex)
+  
+  const line_length = lines[letterIndex]?.length;
+  console.log(line_length)
+  if (letterIndex === line_length -1){
+    console.log("`?",lineIndex)
+    console.log(lines[lineIndex])
+    linesRef._raw_value.push(lines[lineIndex])
+    console.log(lineIndex)
+  }
+
+  return {
+    animationDelay: delay,
+  };
+}
+
+
+
+</script>
+<template>
+  <div class="aText" :style="(fontSize>0)? `font-size:${props.fontSize}em`:''">
+
+    <p
+      v-for="(line, lineIndex) in lines" 
+      :key="lineIndex" 
+      style="padding:0;margin:0"
+    > 
+    <!-- <p> {{lines[lineIndex]}}</p> -->
+        <span
+          v-for="(letter, letterIndex) in line"
+          :key="letterIndex"
+          class="letter"
+          :style="{animationDelay: dLines.slice(0,lineIndex+1).reduce((partialSum, a) => partialSum + a, 0) + dLetter + letterIndex*dLetter + 's'}"
+        >
+          {{  letter  }}
+        </span>
+      </p>
+  </div>
+</template>
+
+<style>
+.letter {
+  opacity: 0;
+  animation: fadeIn 0.1s ease-in-out forwards;
+  transform: translateX(100px);
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+    transform: translateX(100px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+.aText{
+  color: rgb(0, 0, 0);
+  font-size: x-large;
+}
+</style>
